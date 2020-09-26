f394c8f95e735f6c1f019534ba7421c95287f871
agustinbajo <agustin.bajo@wolox.com.ar>
2020-09-26 14:17:17 -0300

    s

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -11,15 +11,7 @@ class BotController < Telegram::Bot::UpdatesController
   end
 
   def start!(data = nil, *)
-    # do_smth_with(data)
-
-    # There are `chat` & `from` shortcut methods.
-    # For callback queries `chat` if taken from `message` when it's available.
-    #response = from ? "Hello #{from['username']}!" : 'Hi there!'
-    # There is `respond_with` helper to set `chat_id` from received message:
     respond_with :message, text: "Hola mundo"
-    # `reply_with` also sets `reply_to_message_id`:
-    #reply_with :photo, photo: File.open('party.jpg')
   end
 
 
